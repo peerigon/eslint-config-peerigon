a6681b53e9d7c077fb3f13e345c088782af880b6
Johannes Ewald <johannes.ewald@peerigon.com>
2017-03-01 01:06:23 +0100

    Add node.js rules
    These rules try to be as close as possible to base.js while omitting the language features that are not supported by the current maintenance version of node.
    See https://github.com/nodejs/LTS and http://node.green/

---
+++ node.js
@@ -0,0 +1,10 @@
+// These rules try to be as close as possible to base.js while omitting the language features that are not supported by the current maintenance version of node.
+// See https://github.com/nodejs/LTS and http://node.green/
+module.exports = {
+    "parserOptions": {
+        "sourceType": "script"
+    },
+    "rules": {
+        "prefer-spread": 0
+    }
+};
