820124fff0691c7f1a7f95f0abf5db420b138154
Johannes Ewald <johannes.ewald@peerigon.com>
2018-03-31 18:52:46 +0200

    feat: Refactor tests rules
    BREAKING CHANGE: The padded-blocks rule has been activated for tests again.

--- tests.js
+++ tests.js
@@ -3,7 +3,8 @@
 
 module.exports = {
     env: {
-        mocha: true
+        mocha: true,
+        jest: true
     },
     rules: {
         /* eslint-enable sort-keys */
@@ -11,19 +12,11 @@ module.exports = {
         "brace-style": ["error", "1tbs", { allowSingleLine: true }],
         // mocha blocks are nested all the way down
         "max-nested-callbacks": "off",
-        // In case the fp rules are used
-        "max-statements": "off",
         // Can increase the readability of a test if simple mocking functions are in one line
         "max-statements-per-line": ["error", { max: 2 }],
-        // In case the fp rules are used, we need to relax them again
-        "no-restricted-syntax": ["error",
-            "WithStatement", // deprecated
-        ], // http://eslint.org/docs/rules/no-restricted-syntax
         // Sometimes it's necessary to check for undefined explicitly
         "no-undefined": "off",
         // chai uses these as assertions
-        "no-unused-expressions": "off",
-        // mocha blocks tend to be more readable with padding
-        "padded-blocks": "off"
+        "no-unused-expressions": "off"
     }
 };
