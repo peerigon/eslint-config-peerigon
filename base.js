9770632c4090a3cf0157a9199d2abc23c99571eb
Johannes Ewald <johannes.ewald@peerigon.com>
2018-08-26 23:33:08 +0200

    fix: Remove deprecated experimentalObjectRestSpread option

--- base.js
+++ base.js
@@ -8,9 +8,7 @@ module.exports = {
     },
     parserOptions: {
         sourceType: "module",
-        ecmaFeatures: {
-            experimentalObjectRestSpread: true,
-        },
+        ecmaVersion: 2018
     },
     plugins: [
         "jsdoc",
