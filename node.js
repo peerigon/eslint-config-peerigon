25957a30ec90dca90cee78925f28ff8cdfea5f3a
Johannes Ewald <johannes.ewald@peerigon.com>
2018-08-26 19:35:11 +0200

    fix: Remove eslint-plugin-security
    We need to remove the plugin again because there were just
    too many false positives.

--- node.js
+++ node.js
@@ -4,8 +4,6 @@
 // These rules try to be as close as possible to base.js while omitting the language features that are not supported by the current active LTS version of node.
 // See https://github.com/nodejs/LTS and http://node.green/
 module.exports = {
-    plugins: ["security"],
-    extends: ["plugin:security/recommended"],
     env: {
         node: true
     },
