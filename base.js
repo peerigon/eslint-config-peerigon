f5d6c9d263798e28f38325a9857be57c74392031
Johannes Ewald <johannes.ewald@peerigon.com>
2019-07-08 22:38:25 +0200

    feat: Increase import/max-dependencies to 20
    There were a lot of reports in modules where it would not be beneficial to split it into separate modules.

--- base.js
+++ base.js
@@ -151,9 +151,9 @@ module.exports = {
         "import/extensions": ["error", "ignorePackages"], // https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/extensions.md
         "import/first": "error", // https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/first.md
         "import/group-exports": "off", // https://github.com/benmosher/eslint-plugin-import/blob/HEAD/docs/rules/group-exports.md
-        // Using more than 15 dependencies in one module is a sign that this module is doing too much.
+        // Using more than 20 dependencies in one module is a sign that this module is doing too much.
         // You should consider splitting that module into different parts.
-        "import/max-dependencies": ["warn", {max: 15}], // https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/max-dependencies.md
+        "import/max-dependencies": ["warn", {max: 20}], // https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/max-dependencies.md
         "import/named": "error", // https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/named.md
         "import/namespace": "error", // https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/namespace.md
         // Is already covered by eslint rule padding-line-between-statements
