a8e4326bd9794c3c2e3fa411b8bf1d760115cd2b
Johannes Ewald <johannes.ewald@peerigon.com>
2019-07-09 10:20:03 +0200

    feat: Add styles/react-jsx-allow-bind

---
+++ styles/react-jsx-allow-bind.js
@@ -0,0 +1,8 @@
+/* eslint sort-keys: ["error", "asc"], quote-props: ["error", "consistent"] */
+/* eslint-disable sort-keys */
+
+module.exports = {
+    rules: {
+        "react/jsx-no-bind": "off",  // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-no-bind.md
+    }
+};
