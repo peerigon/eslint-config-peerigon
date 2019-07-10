3500665c5cf34b9ffb31989a5d5fd38626c187be
Johannes Ewald <johannes.ewald@peerigon.com>
2019-07-10 11:29:28 +0200

    refactor: Introduce globPatterns.js

---
+++ globPatterns.js
@@ -0,0 +1,20 @@
+"use strict";
+
+module.exports = {
+    tests: [
+        "**/*.{test,spec}.*",
+        "**/test{s,}/**",
+    ],
+    tooling: [
+        "config/**",
+        "script{,s}/**", // contains usually npm scripts
+        "tool{,s}/**", // often used for other scripts
+    ],
+    withSideEffects: [
+        "**/*.css",
+        "**/*.less",
+        "**/*.scss",
+        "**/*.sass",
+        "**/*register"
+    ]
+};
