ed93debc7440bbd0e1547e797124e5b1eb5ebd2f
Johannes Ewald <johannes.ewald@peerigon.com>
2019-08-02 15:58:31 +0200

    fix: Disable `react/jsx-no-literals` in tests

--- styles/react-jsx-no-literals.js
+++ styles/react-jsx-no-literals.js
@@ -1,8 +1,18 @@
 /* eslint sort-keys: ["error", "asc"], quote-props: ["error", "consistent"] */
 /* eslint-disable sort-keys */
 
+const globPatterns = require("../globPatterns.js");
+
 module.exports = {
     rules: {
         "react/jsx-no-literals": "error",  // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-no-literals.md
-    }
+    },
+    // TODO: With ESLint 6 you can pass an array of files. Change this once we've updated.
+    overrides: globPatterns.tests.map(testGlobPattern => ({
+        files: testGlobPattern,
+        rules: {
+            // It's quite common in tests to use example strings
+            "react/jsx-no-literals": "off"
+        }
+    })),
 };
