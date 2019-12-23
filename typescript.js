1c008467c1fe5d02a228e5562d114d058ab9d821
Johannes Ewald <johannes.ewald@peerigon.com>
2019-12-23 17:52:04 +0100

    fix: Turn off consistent-type-definitions in d.ts files
    In d.ts files it might be necessary to merge an existing interface

--- typescript.js
+++ typescript.js
@@ -211,7 +211,8 @@ module.exports = {
         {
             files: ["*.d.ts"],
             rules: {
-                "@typescript-eslint/consistent-type-definitions": ["error", "interface"],
+                // In d.ts files it might be necessary to merge an existing interface
+                "@typescript-eslint/consistent-type-definitions": "off",
                 // When someone wants to extend the typings of a third-party module, it might
                 // be necessary to import the module so that TypeScript finds the typings that should be extended.
                 // This is a better alternative than the triple-slash directive
