049cac6997c59f6c093b750700cb72e636c0f7e1
Johannes Ewald <johannes.ewald@peerigon.com>
2020-02-13 18:24:24 +0100

    fix(react): Fix minor issue when using styles/react-jsx-no-literals
    Reset react/jsx-curly-brace-presence back to the original rule in tests

--- styles/react-jsx-no-literals.js
+++ styles/react-jsx-no-literals.js
@@ -24,6 +24,7 @@ module.exports = {
             rules: {
                 /* eslint-enable sort-keys */
                 // It's quite common in tests to use example strings
+                "react/jsx-curly-brace-presence": ["warn", "never"],
                 "react/jsx-no-literals": "off",
                 /* eslint-disable sort-keys */
             }
