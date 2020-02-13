993568b6884b068407e01443d8551be4467dc8d6
Johannes Ewald <johannes.ewald@peerigon.com>
2020-02-13 17:02:25 +0100

    fix(react): Fix problem with escape hatch in styles/react-jsx-no-literals
    If we don't adjust react/jsx-curly-brace-presence, it would autofix the escape hatch {"some string"} allowed by "jsx-no-literals"

--- styles/react-jsx-no-literals.js
+++ styles/react-jsx-no-literals.js
@@ -6,6 +6,15 @@ const globPatterns = require("../glob-patterns.js");
 module.exports = {
     rules: {
         /* eslint-enable sort-keys */
+        // If we don't adjust this rule, it would autofix the escape hatch
+        // {"some string"} allowed by "jsx-no-literals"
+        "react/jsx-curly-brace-presence": [
+            "warn",
+            {
+                "children": "always",
+                "props": "never",
+            }
+        ],
         "react/jsx-no-literals": "warn",  // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-no-literals.md
         /* eslint-disable sort-keys */
     },
@@ -15,7 +24,7 @@ module.exports = {
             rules: {
                 /* eslint-enable sort-keys */
                 // It's quite common in tests to use example strings
-                "react/jsx-no-literals": "off"
+                "react/jsx-no-literals": "off",
                 /* eslint-disable sort-keys */
             }
         }
