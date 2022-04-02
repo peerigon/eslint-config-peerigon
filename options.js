36e61dfa1b4791253213043807f38da1916a119b
Johannes Ewald <johannes.ewald@peerigon.com>
2022-04-02 18:47:26 +0200

    fix(TypeScript): Relax @typescript-eslint/naming-convention

--- options.js
+++ options.js
@@ -109,40 +109,67 @@ module.exports = {
     ["@typescript-eslint/naming-convention"]: (() => {
         const options = {
             default: {
+                selector: "default",
                 format: ["camelCase", "PascalCase", "UPPER_CASE"],
                 leadingUnderscore: "allow",
-                selector: "default",
                 trailingUnderscore: "allow",
             },
             function: {
+                selector: "function",
                 format: ["camelCase"],
                 leadingUnderscore: "allow",
-                selector: "function",
                 trailingUnderscore: "allow",
             },
             parameter: {
+                selector: "parameter",
                 format: ["camelCase", "PascalCase"],
                 leadingUnderscore: "allow",
-                selector: "parameter",
                 trailingUnderscore: "allow",
             },
             method: {
+                selector: "method",
                 format: ["camelCase"],
                 leadingUnderscore: "allow",
-                selector: "method",
                 trailingUnderscore: "allow",
             },
             typeLike: {
+                selector: "typeLike",
                 format: ["PascalCase"],
                 leadingUnderscore: "allow",
-                selector: "typeLike",
                 trailingUnderscore: "allow",
             },
             enumMember: {
+                selector: "enumMember",
                 format: ["PascalCase"],
                 leadingUnderscore: "allow",
-                selector: "enumMember",
                 trailingUnderscore: "allow",
+            },
+            // In case it's destructured, we don't want to force the developer to change the casing
+            destructuring: {
+                selector: "variable",
+                modifiers: ["destructured"],
+                format: null,
+            },
+            // In case the property name requires quotes, we don't enforce any convention
+            requiresQuites: {
+                selector: [
+                    "classProperty",
+                    "objectLiteralProperty",
+                    "typeProperty",
+                    "classMethod",
+                    "objectLiteralMethod",
+                    "typeMethod",
+                    "accessor",
+                    "enumMember",
+                ],
+                modifiers: ["requiresQuotes"],
+                format: null,
+            },
+            // We don't enforce any convention on object literals since these are often used
+            // for data object where we're not in control of the type (e.g. an api that takes camel_case properties)
+            objectLiteralProperty: {
+                selector: "objectLiteralProperty",
+                format: null,
             },
         };
 
