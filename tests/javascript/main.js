989c540ade673072b9aed441e6c304fe7eeeff10
Johannes Ewald <johannes.ewald@peerigon.com>
2023-11-06 17:19:49 +0100

    Fix test

--- tests/javascript/main.js
+++ tests/javascript/main.js
@@ -2,6 +2,10 @@ import { getMessage } from "./message.js";
 
 class SomeClass {
     #someProp = true;
+
+    logSomeProp() {
+        console.log(this.#someProp);
+    }
 }
 
 console.log(getMessage(), SomeClass);
