501c74e86767cef936ac96bc2c9757eb4f3690ae
Johannes Ewald <johannes.ewald@peerigon.com>
2024-04-29 17:00:59 +0200

    fix: Disable @trivago/prettier-plugin-sort-imports temporarily (#142)
    See https://github.com/peerigon/eslint-config-peerigon/issues/141

--- prettier.config.js
+++ prettier.config.js
@@ -1,6 +1,9 @@
 "use strict";
 
 module.exports = {
-    plugins: ["@trivago/prettier-plugin-sort-imports"],
-    importOrder: ["^node:", "<THIRD_PARTY_MODULES>", "^[./]"],
+    // TODO: Re-add this plugin once it's fixed, https://github.com/peerigon/eslint-config-peerigon/issues/141
+    // This plugin breaks with import attributes like `import ... with { type: "json" }
+    // See https://github.com/trivago/prettier-plugin-sort-imports/issues/270
+    // plugins: ["@trivago/prettier-plugin-sort-imports"],
+    // importOrder: ["^node:", "<THIRD_PARTY_MODULES>", "^[./]"],
 };
